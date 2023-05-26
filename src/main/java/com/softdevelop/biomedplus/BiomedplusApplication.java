acb053e3e053f055a4ee93703fa70ba830a737a3
Alexander Cardenas <alexcardenas.u@gmail.com>
2023-05-25 22:57:58 -0500

    First commit

---
+++ src/main/java/com/softdevelop/biomedplus/BiomedplusApplication.java
@@ -0,0 +1,13 @@
+package com.softdevelop.biomedplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiomedplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiomedplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softdevelop/biomedplus/BiomedplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softdevelop.biomedplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiomedplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
