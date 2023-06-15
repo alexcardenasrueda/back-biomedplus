87ba3f9142ee610675426dc3af25e3c10805a608
Anyi Bustos <anyi.bustos@mercadolibre.com.co>
2023-06-14 23:27:41 -0500

    Cross origin

--- src/main/java/com/softdevelop/biomedplus/controller/RepuestoController.java
+++ src/main/java/com/softdevelop/biomedplus/controller/RepuestoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.Objects;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/repuestos")
 @RequiredArgsConstructor
