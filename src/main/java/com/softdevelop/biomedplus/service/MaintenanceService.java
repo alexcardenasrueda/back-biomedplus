94742e98f49e9bf8b4e34bdb0fa59b7706ac0041
Anyi Bustos <anyi.bustos@mercadolibre.com.co>
2023-08-17 20:58:16 -0500

    create and update maintenance

--- src/main/java/com/softdevelop/biomedplus/service/MaintenanceService.java
+++ src/main/java/com/softdevelop/biomedplus/service/MaintenanceService.java
@@ -2,6 +2,7 @@ package com.softdevelop.biomedplus.service;
 
 import com.softdevelop.biomedplus.exception.GenericException;
 import com.softdevelop.biomedplus.model.dto.EquipmentDto;
+import com.softdevelop.biomedplus.model.dto.MaintenanceDto;
 import com.softdevelop.biomedplus.model.dto.NextMaintenanceEquipmentDto;
 
 import java.util.List;
@@ -9,5 +10,9 @@ import java.util.List;
 public interface MaintenanceService {
 
   List<NextMaintenanceEquipmentDto> nextExpected();
+
+  Long createMaintenance(MaintenanceDto maintenanceDto);
+
+  MaintenanceDto updateMaintenance(Long id, MaintenanceDto maintenanceDto);
 
 }
